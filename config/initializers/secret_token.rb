e253222f577f70d3bce2416ca41705ca8551d785
gunhyung <gunhyung@snuev.com>
2013-04-21 21:56:19 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cup::Application.config.secret_token = 'cf16f350cc270fc3004573dfda42272dcbbbb19bbbee6bbc9db60298d5cd5b5fd3ef10cf63b191616bfdb5fc5ccf5ff4368df34605ab4c84ce9db3157f1a4308'
